141e1dcdd271b18252966efc14914ac1d85914bb
fazzaamiarso <fazzaamiarso@gmail.com>
2022-02-19 03:36:36 +0700

    Setup linting and pre-commit hook

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,14 +1,14 @@
-import { useLoaderData } from "remix";
+import { useLoaderData } from 'remix';
 
 export const loader = async () => {
-  return "I came from loader!";
+  return 'I came from loader!';
 };
 
 export default function Index() {
   const data = useLoaderData();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1 className="text-red-500 text-2xl">Hello hackathooon!!</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1 className="text-2xl text-red-500">Hello hackathooon!!</h1>
       <p>{data}</p>
     </div>
   );
