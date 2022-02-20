a51fd6a24918cf8992c44c0f5936770d598da0c4
fazzaamiarso <fazzaamiarso@gmail.com>
2022-02-20 23:39:43 +0700

    Connect to supabase

---
+++ app/routes/post.new.jsx
@@ -0,0 +1,5 @@
+export const action = async () => {};
+
+export default function NewPost() {
+  return <h1 className="text-2xl">Add your thought</h1>;
+}
--- app/routes/search/index.jsx
+++ app/routes/search/index.jsx
@@ -1,9 +1,35 @@
+import { useLoaderData } from 'remix';
+import { supabase } from '../../../server/db.server';
+
+export const loader = async ({ request }) => {
+  const newUrl = new URL(request.url);
+  const searchTerm = newUrl.searchParams.get('term');
+
+  if (searchTerm === null) {
+    const { data } = await supabase
+      .from('post')
+      .select()
+      .order('created_at', { ascending: false });
+    return { data };
+  }
+  return {};
+};
+
 export default function SearchPost() {
+  const { data } = useLoaderData();
   return (
     <>
       <h2 className="">Search post</h2>
       <ul className="">
-        <li className="">No Post Yet!!</li>{' '}
+        {data.map(post => {
+          return (
+            <li key={post.id} className="">
+              <h3 className="text-lg">{post.track_id}</h3>
+              <p>{post.lyrics}</p>
+              <p>{post.thought}</p>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
