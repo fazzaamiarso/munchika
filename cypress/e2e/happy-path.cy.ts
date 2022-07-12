2b87526533d063ab273893e27b57ce6d3684df71
fazzaamiarso <fazzaamiarso@gmail.com>
2022-07-13 05:07:19 +0700

    test(setup): Integrate testing-library with cypress

--- cypress/e2e/happy-path.cy.ts
+++ cypress/e2e/happy-path.cy.ts
@@ -1,4 +1,6 @@
 it('Run the test!!', () => {
   cy.visit('/');
-  expect(true).equal(true);
+  cy.findByRole('heading', { level: 1, name: /thought/i });
+  cy.findByRole('link', { name: /add a thought/i }).click();
+  cy.location('pathname').should('contain', '/post/select');
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,6 +11,9 @@
 // ***********************************************
 //
 //
+
+import '@testing-library/cypress/add-commands';
+
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
